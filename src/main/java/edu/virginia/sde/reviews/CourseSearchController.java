415ba4ce85ab0983ea13527e75b6bd9dc76e2e16
Luke McMeans <elukemcmeans@gmail.com>
2023-12-05 13:27:39 -0500

    Fixed error within searched courses where new searches wouldn't pull from all courses in the database

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -3,7 +3,6 @@ package edu.virginia.sde.reviews;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -28,19 +27,25 @@ public class CourseSearchController {
     @FXML
     private TextField title;
 
-    private final Catalog catalog = new Catalog();
+    private Catalog allCourses = new Catalog();
+
+    private Catalog searchCourses = new Catalog();
 
     private Course selected;
 
     public void initialize() {
+        createAllCourses();
+        showAllCourses();
+        tableView.setOnMouseClicked(this::handleCourseSelect);
+        tableView.setOnMouseEntered(this::handleHoverEnter);
+        tableView.setOnMouseExited(this::handleHoverExit);
+    }
+
+    public void clearSearch(){
         showAllCourses();
         mnemonic.clear();
         courseNumber.clear();
         title.clear();
-        tableView.setOnMouseClicked(this::handleCourseSelect);
-        tableView.setOnMouseEntered(this::handleHoverEnter);
-        tableView.setOnMouseExited(this::handleHoverExit);
-
     }
 
     private void handleHoverExit(MouseEvent mouseEvent) {
@@ -75,7 +80,7 @@ public class CourseSearchController {
 
     private void updateTable() {
         if (tableView != null) {
-            ObservableList<Course> obsList = FXCollections.observableList(catalog.getCoursesInAlphabetMnemonicOrder());
+            ObservableList<Course> obsList = FXCollections.observableList(searchCourses.getCoursesInAlphabetMnemonicOrder());
             Session session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
             for (Course c : obsList) {
@@ -90,34 +95,64 @@ public class CourseSearchController {
     }
 
     public void handleSearchButton() {
-        if (tableView.getItems() != null) {
-            catalog.setCourses(tableView.getItems());
-        } else {
+        if (tableView.getItems() == null || tableView.getItems().isEmpty() || !validInputs()) {
             return;
         }
         if (!this.mnemonic.getText().isEmpty() || !this.courseNumber.getText().isEmpty() || !this.title.getText().isEmpty()) {
+            searchCourses.setCourses(allCourses.getCoursesInAlphabetMnemonicOrder());
             if (!this.mnemonic.getText().isEmpty()) {
-                catalog.setCourses(catalog.getCoursesByMnemonic(mnemonic.getText()));
+                searchCourses.setCourses(searchCourses.getCoursesByMnemonic(mnemonic.getText().trim()));
             }
             if (!this.courseNumber.getText().isEmpty()) {
-                catalog.setCourses(catalog.getCoursesByNumber(Integer.parseInt(courseNumber.getText())));
+                searchCourses.setCourses(searchCourses.getCoursesByNumber(Integer.parseInt(courseNumber.getText().trim())));
             }
             if (!this.title.getText().isEmpty()) {
-                catalog.setCourses(catalog.getCoursesByTitle(title.getText()));
+                searchCourses.setCourses(searchCourses.getCoursesByTitle(title.getText()));
             }
-        } else {
-            showAllCourses();
+            updateTable();
         }
-        updateTable();
     }
 
-    public void showAllCourses() {
+    private boolean validInputs(){
+        if (!mnemonic.getText().trim().isEmpty()) {
+            if(mnemonic.getText().trim().length() > 4 || mnemonic.getText().trim().length() < 2) {
+                return false;
+            }
+            for(int i = 0; i < mnemonic.getText().trim().length(); i++){
+                if(mnemonic.getText().trim().toUpperCase().charAt(i) > 90 || mnemonic.getText().trim().toUpperCase().charAt(i) < 65){
+                    return false;
+                }
+            }
+        }
+        if (!courseNumber.getText().trim().isEmpty()) {
+            for(int i = 0; i < courseNumber.getText().length(); i++){
+                if(!Character.isDigit(courseNumber.getText().charAt(i))){
+                    return false;
+                }
+            }
+            if(Integer.parseInt(courseNumber.getText().trim()) < 1000 || Integer.parseInt(courseNumber.getText().trim()) > 9999) {
+                return false;
+            }
+        }
+        if (!title.getText().isEmpty()) {
+            if(title.getText().trim().isEmpty() || title.getText().trim().length() > 50){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void createAllCourses() {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-        catalog.setCourses(session.createQuery("FROM Course", Course.class).list());
+        allCourses.setCourses(session.createQuery("FROM Course", Course.class).list());
         session.getTransaction().commit();
         session.close();
+    }
+
+    public void showAllCourses() {
+        searchCourses.setCourses(allCourses.getCoursesInAlphabetMnemonicOrder());
         updateTable();
     }
 
